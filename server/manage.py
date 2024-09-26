606a726d2a9db2da9f92ae3437046cd304f4e1ce
KuldeepMahto <71553879+KuldeepMahto07@users.noreply.github.com>
2024-09-26 18:07:51 +0530

    Update manage.py

--- server/manage.py
+++ server/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management import execute_from_command_line
 
 def main():
     """Run administrative tasks."""
@@ -20,3 +20,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+import os
+from django.core.management import execute_from_command_line
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+    execute_from_command_line(['manage.py', 'runserver', '0.0.0.0:' + os.environ.get('PORT', '8000')])
+
